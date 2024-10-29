820dac7576b2734f1338278db1da7add72d67705
IntOflnxoverxdx <106513380+IntOflnxoverxdx@users.noreply.github.com>
2024-10-29 18:40:30 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,252 @@
+rofl = false
+
+
+songs.forEach(song => {
+    document.querySelector(".album-covers").innerHTML += `
+    <img src="${song.cover}" alt="">
+    `
+});
+
+
+currentSong = 0
+function setSong(autoplay = false){
+    seconds = (songs[currentSong].length%60).toString()
+    if (seconds.length < 2){
+        seconds = "0"+seconds
+    }
+    document.querySelector(".total-time").innerHTML = `${Math.floor(songs[currentSong].length/60)}:${seconds}`
+    soundPlayer = new Audio(songs[currentSong].file)
+    document.querySelector(".now-time").innerHTML = `0:00`
+    document.querySelector("body").style = `background-color:${songs[currentSong].color};`
+    document.querySelector(".songname").innerHTML = songs[currentSong].name
+    document.querySelector(".author").innerHTML = songs[currentSong].author
+    
+    document.querySelectorAll(".album-covers img").forEach((cover,i) => {
+        cover.style.left = `calc(${(i-currentSong)*100}% + 20px)`
+    });
+
+    update(soundPlayer.currentTime)
+
+    if (autoplay){soundPlayer.play()}
+}
+
+
+function update(time){
+    minutes = Math.floor(time/60)
+    seconds = Math.floor(time%60).toString()
+    if (seconds.length < 2){
+        seconds = "0"+seconds
+    }
+    document.querySelector(".now-time").innerHTML = `${minutes}:${seconds}`
+    document.querySelector(".progress").style.width = `${time/songs[currentSong].length*100}%`
+    document.querySelector(".circle").style.left = `${time/songs[currentSong].length*100}%`
+}
+
+
+
+function checkEnded(){
+    if (soundPlayer.currentTime >= songs[currentSong].length){
+        if (currentSong == songs.length-1){
+            currentSong = 0;
+        } 
+        else{
+            currentSong += 1
+        }
+        if (!pauseButton.classList.contains("paused")){
+            soundPlayer.pause()
+            setTimeout(() => {
+                setSong(true)
+            }, 50);
+        }else{
+            setSong()
+        }
+        soundPlayer.pause()
+    }
+}
+function nextSong(){
+    if (currentSong == songs.length-1){
+        currentSong = 0;
+    } 
+    else{
+        currentSong += 1
+    }
+    if (!pauseButton.classList.contains("paused")){
+        soundPlayer.pause()
+        setTimeout(() => {
+            setSong(true)
+        }, 50);
+    }else{
+        setSong()
+    }
+    soundPlayer.pause()
+}
+function previousSong(){
+    if (currentSong == 0){
+        currentSong = songs.length-1;
+    } 
+    else{
+        currentSong -= 1
+    }
+    if (!pauseButton.classList.contains("paused")){
+        soundPlayer.pause()
+        setTimeout(() => {
+            setSong(true)
+        }, 50);
+    }else{
+        setSong()
+    }
+    soundPlayer.pause()
+}
+
+
+function nextControls(){    
+    document.querySelector(".bottom").innerHTML += `
+            <div class="controls c2" style="transform:translateX(120%);">
+                <div class="songname">${songs[currentSong].name}</div>
+                <div class="author">${songs[currentSong].author}</div>
+                <div class="timeline">
+                    <div class="line">
+                        <div class="progress" style="width:${time/songs[currentSong].length*100}%"></div>
+                        <div class="circle" style="left:${time/songs[currentSong].length*100}%"></div>
+                    </div>
+                    <div class="now-time">
+                        0:00
+                    </div>
+                    <div class="total-time">
+                        0:00
+                    </div>
+                </div>
+
+                <div class="control-buttons">
+                    <div class="like button">
+    
+                    </div>
+                    <div class="song-controls">
+                        <div class="previous button"></div>
+                        <div class="pause button ${document.querySelector(".pause").classList.contains("paused")?"paused":""}"></div>
+                        <div class="next button"></div>
+                    </div>
+                    <div class="dislike button">
+    
+                    </div>
+                </div>
+            </div>
+    `
+    setTimeout(() => {
+        document.querySelector(".controls").style = "transition:.3s; transform:translate(-120%,0);"
+        document.querySelector(".controls.c2").style = "transition:.3s; transform:translate(0,0);"
+    }, 1);
+    setTimeout(() => {
+        document.querySelector(".controls").style = "transition:0;"
+        document.querySelector(".c2").remove()
+        setButtons()
+    }, 300);
+}
+
+function previousControls(){    
+    document.querySelector(".bottom").innerHTML = `
+            <div class="controls c2" style="transform:translateX(-120%);">
+                <div class="songname">${songs[currentSong].name}</div>
+                <div class="author">${songs[currentSong].author}</div>
+                <div class="timeline">
+                    <div class="line">
+                        <div class="progress" style="width:${time/songs[currentSong].length*100}%"></div>
+                        <div class="circle" style="left:${time/songs[currentSong].length*100}%"></div>
+                    </div>
+                    <div class="now-time">
+                        0:00
+                    </div>
+                    <div class="total-time">
+                        0:00
+                    </div>
+                </div>
+
+                <div class="control-buttons">
+                    <div class="like button">
+    
+                    </div>
+                    <div class="song-controls">
+                        <div class="previous button"></div>
+                        <div class="pause button ${document.querySelector(".pause").classList.contains("paused")?"paused":""}"></div>
+                        <div class="next button"></div>
+                    </div>
+                    <div class="dislike button">
+    
+                    </div>
+                </div>
+            </div>
+    ` + document.querySelector(".bottom").innerHTML
+    
+    setTimeout(() => {
+        document.querySelectorAll(".controls")[1].style = "transition:.3s; transform:translate(120%,0);"
+        document.querySelector(".controls.c2").style = "transition:.3s; transform:translate(0,0);"
+    }, 1);
+    setTimeout(() => {
+        document.querySelectorAll(".controls")[1].style = "transition:0;"
+        document.querySelector(".c2").remove()
+        setButtons()
+    }, 300);
+}
+
+
+setInterval(() => {
+    update(soundPlayer.currentTime)
+    checkEnded()
+}, 1000);
+
+
+
+setSong()
+
+
+let time = 0
+
+
+function setButtons(){
+    document.querySelector(".next").onclick = ()=>{
+        if (!rofl){
+            nextSong()
+        }
+        else{
+            nextControls()
+        }
+    }
+    document.querySelector(".previous").onclick = ()=>{
+        if (!rofl){
+            previousSong()
+        }
+        else{
+            previousControls()
+        }
+    }
+    timeline = document.querySelector(".timeline")
+    timeline.addEventListener("click",(event)=>{
+        time = Math.floor(event.offsetX/timeline.offsetWidth*songs[currentSong].length)
+        
+        soundPlayer.currentTime = time
+        update(time)
+    })
+    pauseButton = document.querySelector(".pause")
+    pauseButton.onclick = ()=>{
+        if (pauseButton.classList.contains("paused")){
+            soundPlayer.play()
+        }else{
+            soundPlayer.pause() 
+        }
+        pauseButton.classList.toggle("paused")
+    }
+}
+
+
+document.querySelector(".rofl").onclick = ()=>{
+    if (document.querySelector("#rofl").checked){
+        rofl = true
+    }else{
+        rofl = false
+    }
+    setButtons()
+}
+
+
+
+setButtons()
---
+++ songs.js
@@ -0,0 +1,59 @@
+songs = [
+    {
+        name:"Я бью женщин и детей",
+        author:"Валентин Стрыкало",
+        file:"media/music/Я бью женщин и детей.mp3",
+        cover:"media/albums/Смирись и расслабься.png",
+        length:161,
+        color:"#9a996e"
+    },
+    {
+        name:"Метафизика",
+        author:"Anacondaz, Заточка",
+        file:"media/music/Метафизика.mp3",
+        cover:"media/albums/Перезвони.jpg",
+        length:263,
+        color:"#666"
+    },
+    {
+        name:"BOOM",
+        author:"fleeboosta",
+        file:"media/music/BOOM.mp3",
+        cover:"media/albums/boom.jpg",
+        length:117,
+        color:"#e0b050"
+    },
+    {
+        name:"killua",
+        author:"zxcursed",
+        file:"media/music/killua.mp3",
+        cover:"media/albums/killua.jpg",
+        length:97,
+        color:"#c2543c"
+    },
+    {
+        name:"Ничего Святого",
+        author:"хмыров",
+        file:"media/music/Ничего Святого.mp3",
+        cover:"media/albums/ничего святого.jpg",
+        length:181,
+        color:"#a4d57c"
+    },
+    {
+        name:"В МЕТРО",
+        author:"Земфира",
+        file:"media/music/В МЕТРО.mp3",
+        cover:"media/albums/спасибо.jpg",
+        length:172,
+        color:"#999"
+    },
+    {
+        name:"Вселенная бесконечна?",
+        author:"Noize MC",
+        file:"media/music/Вселенная бесконечна.mp3",
+        cover:"media/albums/новый альбом.jpg",
+        length:260,
+        color:"#888"
+    },
+]
+songs.sort(() => Math.random() - 0.5);
